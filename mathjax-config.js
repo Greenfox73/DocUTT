4159de14256eb9d0705b918920eadba96eda72ce
Jean-Sébastien RIGAUD <jeanseb73.pro@gmail.com>
2026-01-09 00:23:22 +0100

    Deployed 0b9a4a3 with MkDocs version: 1.6.1

---
+++ mathjax-config.js
@@ -0,0 +1,6 @@
+window.MathJax = {
+  tex: {
+    displayAlign: "left",   // aligne les blocs $$ ... $$ à gauche
+    displayIndent: "0em"    // pas de retrait (tu peux mettre "2em" si tu veux un petit décalage)
+  }
+};
